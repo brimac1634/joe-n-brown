3bde2a4d0fd1664c0a02c34e248303a73668b8d6
Brian MacPherson <brimac1634@gmail.com>
2021-02-16 14:49:26 +0800

    updated information in index html and header name

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC<HeaderProps> = () => {
     return ( 
         <header className='w-screen text-center py-1 flex-none'>
             <div className='flex items-center flex-col'>
-                <Link to='/' className='text-xl lg:text-2xl font-semibold py-1 md:py-2'>Joe N. Brown</Link>
+                <Link to='/' className='text-xl lg:text-2xl font-semibold py-1 md:py-2'>Joe N. Brown Art</Link>
                 <div className='w-3/5 px-24 max-w-lg h-0.5 bg-black'/>
             </div>
         </header>
